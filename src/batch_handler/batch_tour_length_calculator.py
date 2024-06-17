7e58a57c421e5f2351867c3835a4a88dc445f206
PSchmidt <philipp.schmidt@odenwaldquelle.de>
2024-06-17 20:18:04 +0200

    Improved tests and algorithm. Still not working properly.

--- src/batch_handler/batch_tour_length_calculator.py
+++ src/batch_handler/batch_tour_length_calculator.py
@@ -11,7 +11,7 @@ def calculate_tour_length_s_shape_routing(batch, warehouse_layout):
     '''
 
     # Sort the batch by the x-coordinate, y-coordinate, and z-coordinate
-    sorted_batch = sort_batch_s_shape_routing(batch)
+    sorted_batch = sort_and_transform_batch_s_shape_routing(batch)
     #Store the warehouse layout information as a maximum y and z position and the maximum x position transformed
     max_x_position_transformed = round(warehouse_layout['max_x_position']/2,0)
     max_y_position = warehouse_layout['max_y_position']
@@ -20,15 +20,17 @@ def calculate_tour_length_s_shape_routing(batch, warehouse_layout):
     # Initialize the total tour length
     total_tour_length = 0
     # Initialize the current position	
-    starting_position = (0, 0, 0)
+    starting_position = (0, -1, 0) # The starting position is one unit below the first aisle
     current_position = starting_position
 
     for item in sorted_batch:
         # Get the item position
-        item_position = (item['x_position'], item['y_position'], item['z_position'])
+        item_position = (item['abs_x_position'], item['abs_y_position'], item['abs_z_position'])
 
         # Calculate the distance between the current position and the item position
-        distance = calculate_distance_to_next_item(current_position, item_position, max_y_position)
+        distance = calculate_distance_to_next_item(current_position, starting_position, item_position, max_y_position)
+        # Update the current position
+        current_position = item_position
         # Update the total tour length
         total_tour_length += distance
 
@@ -37,35 +39,35 @@ def calculate_tour_length_s_shape_routing(batch, warehouse_layout):
     # Update the total tour length
     total_tour_length += distance
 
-    return total_tour_length
+    return total_tour_length, sorted_batch
 
 
 
-def sort_batch_s_shape_routing(unsorted_batch):
+def sort_and_transform_batch_s_shape_routing(unsorted_batch):
     """
-    This function sorts the batch primarily by the x-coordinate, secondarily by the y-coordinate, and lastly by the z-coordinate. Additionally, it transforms the x-coordinate so that rows which are on the opposite side of each other and only need to traversed once will be regarded as one.
+    This function sorts the batch primarily by the x-coordinate, secondarily by the y-coordinate, and lastly by the z-coordinate. Additionally, it transforms the x-coordinate so that aisles which are on the opposite side of each other and only need to traversed once will be regarded as one.
 
     :param unsorted_batch: A list of dictionaries, each containing 'id', 'abs_x_position', 'abs_y_position', and 'abs_z_position'.
     :return: The sorted batch.
     """
-    # Sort the batch by the x-coordinate, y-coordinate, and z-coordinate
-    sorted_batch = sorted(unsorted_batch, key=lambda x: (x['abs_x_position'], x['abs_y_position'], x['abs_z_position']))
     transformed_batch = []
-    for item in sorted_batch:
+    for item in unsorted_batch:
         # Transform the x-coordinate
         transformed_item=item.copy()
         if transformed_item['abs_x_position']%2==0:
             # If the x-coordinate is even, divide it by 2
             transformed_item['abs_x_position']=(transformed_item['abs_x_position'])/2
         else:
-            # If the x-coordinate is odd, subtract 1 and divide it by 2, so that it has the same value as the opposite row
+            # If the x-coordinate is odd, subtract 1 and divide it by 2, so that it has the same value as the opposite aisle
             transformed_item['abs_x_position']=(transformed_item['abs_x_position']-1)/2
         transformed_batch.append(transformed_item)
-    return sorted(transformed_batch,key=lambda x: (x['abs_x_position']))
+    # Sort the batch by the x-coordinate, y-coordinate, and z-coordinate
+    return sorted(transformed_batch, key=lambda x: (x['abs_x_position'], x['abs_y_position'], x['abs_z_position']))
 
 
 
-def calculate_distance_to_next_item(current_position, item_position, max_y_position):
+
+def calculate_distance_to_next_item(current_position, starting_position, item_position, max_y_position):
     """
     This function calculates the distance between the current position and the item position in the warehouse.
 
@@ -75,29 +77,34 @@ def calculate_distance_to_next_item(current_position, item_position, max_y_posit
     :return: The distance between the current position and the item position.
     """
     
+    # Declare the global variables
+    global direction
+    
     if(current_position[0]==item_position[0]):
-            #If the item is on the same row as the current position
-            distance=abs(current_position[1]-item_position[1])
+            #If the item is on the same aisle as the current position
+            distance=abs(current_position[1]-item_position[1]) #Calculate the y-distance to the item
     else:
-            #If the item is on a different row than the current position
+            #If the item is on a different aisle than the current position
             if(direction):
                 #If the direction is upwards
-                #Calculate the y-distance to the end of the row
-                 distance=abs(current_position[1]-max_y_position)
+                #Calculate the y-distance to the end of the aisle. As the y-coordinate starts at 0, we have to do one more step to get to the end of the aisle
+                 distance=abs(current_position[1]-(max_y_position))
                 #Calculate the x-distance to the item
                  distance+=abs(current_position[0]-item_position[0])
-                #Calculate the y-distance to the item from the end of the row 
-                 distance+=abs(max_y_position-item_position[1])
+                #Calculate the y-distance to the item from the end of the aisle.
+                 distance+=abs((max_y_position)-item_position[1])
                 #Change the direction
                  direction=False
             else:
                 #If the direction is downwards
-                #Calculate the y-distance to the end of the row
-                distance=abs(current_position[1]-0)
+                #Calculate the y-distance to the end of the aisle. As the y-coordinate starts at 0, we have to do one more step to get to the end of the aisle
+                distance=abs(current_position[1]-starting_position[1])
                 #Calculate the x-distance to the item
                 distance+=abs(current_position[0]-item_position[0])
-                #Calculate the y-distance to the item from the end of the row
-                distance+=abs(0-item_position[1])
+                #Calculate the y-distance to the item from the end of the aisle.
+                distance+=abs(starting_position[1]-item_position[1])
+                #Change the direction
+                direction=True
 
     return distance
 
--- tests/batch_handler/test_batch_tour_length_handler.py
+++ tests/batch_handler/test_batch_tour_length_handler.py
@@ -1,5 +1,7 @@
 import unittest
-from src.batch_tour_length_calculator import calculate_tour_length_s_shape_routing
+import sys
+import unittest
+from src.batch_handler.batch_tour_length_calculator import calculate_tour_length_s_shape_routing
 from tests.data.test_batch_10 import test_batches, warehouse_layout
 
 class TestBatchTourLengthHandler(unittest.TestCase):
@@ -7,7 +9,10 @@ class TestBatchTourLengthHandler(unittest.TestCase):
     def test_calculate_tour_length_s_shape_routing(self):
         for i, batch in enumerate(test_batches):
             with self.subTest(batch=i):
-                result = calculate_tour_length_s_shape_routing(batch, warehouse_layout)
+                result, sorted_batch = calculate_tour_length_s_shape_routing(batch, warehouse_layout)
+                print(f"Batch {i} tour length: {result}")
+                print(f"Batch {i} sorted batch: {sorted_batch}")
+                print("\n")
                 self.assertIsInstance(result, float, f"Test batch {i} failed: Result is not a float")
 
 if __name__ == '__main__':
