f9de85dbd83826db8e8867d208457a32567da5ac
PSchmidt <philipp.schmidt@odenwaldquelle.de>
2024-06-25 21:32:13 +0200

    Enhancement: Development of a Real-Time Batching Algorithm for Order Picker Optimization #10: Initial commit

---
+++ src/on_line_handler/real_time_decision_handler.py
@@ -0,0 +1,18 @@
+#TODO: Implement the following function
+def picker_state():
+    pass
+
+
+#TODO: Implement the following function
+def order_picking_decision_point_ab(orders, max_batch_size, warehouse_layout, rearrangement_parameter, threshold_parameter, time_limit):
+   pass
+
+
+#TODO: Implement the following function
+def order_picking_decision_point_c(batches, max_batch_size, warehouse_layout, rearrangement_parameter, threshold_parameter, time_limit):
+    pass
+
+
+#TODO: Implement the following function
+def select_one_batch(batches):
+    pass
